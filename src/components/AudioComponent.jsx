693e72c0624a0ddf49aa629e7222ea8c99c66409
Iheb-AG <101937924+Iheb-AG@users.noreply.github.com>
2023-07-24 00:50:55 +0100

    enhanced logic + icons added

--- src/components/AudioComponent.jsx
+++ src/components/AudioComponent.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState , useRef } from "react";
+import {BiPause, BiPlay, BiSkipNext, BiSkipPrevious} from 'react-icons/bi'
 
 export default function AudioComponent(props){
   //creating player refrence
@@ -6,8 +7,10 @@ export default function AudioComponent(props){
   const urlPlaylist = props.urlPlaylist
   const [trackstate,setTrackState] = useState("play")
   const [currentTrackUrl,setCurrectTrackUrl] = useState(props.urlPlaylist[0] ? props.urlPlaylist[0] : null)
+  const [duration,setDuration] = useState(0)
+  const [currentTime,setcurrentTime] = useState(0)
 
-  console.log("current Track url : ",currentTrackUrl)
+  // console.log("current Track url : ",currentTrackUrl)
   function togglePlay(e){
     if(trackstate=="play"){
       setTrackState("pause")
@@ -15,24 +18,51 @@ export default function AudioComponent(props){
     }else{
       setTrackState("play")
       AudioRef.current.pause()
+      console.log(AudioRef)
     }
   }
 
+  
   //getting the next/previous track from url list depending on the step provided (previous track : step = -1 , next track : step = +1)
   function handleNext(){
-    setCurrectTrackUrl(urlPlaylist[(urlPlaylist.indexOf(currentTrackUrl)+1)%urlPlaylist.length])     
+    AudioRef.current.pause()
+    setCurrectTrackUrl(urlPlaylist[(urlPlaylist.indexOf(currentTrackUrl)+1)%urlPlaylist.length])   
+    setTrackState("play")  
   }
 
   function handlePrevious(){
+    AudioRef.current.pause()
     setCurrectTrackUrl(urlPlaylist[Math.abs((urlPlaylist.indexOf(currentTrackUrl)-1))%urlPlaylist.length])
+    setTrackState("play")
   }
+
+  // useEffect(()=>{
+  //   setDuration(AudioRef.current.duration)
+
+  // },[AudioRef.current.currentTime])
 
   return (
     <div>
-        <audio src={currentTrackUrl} controls ref={AudioRef} /> 
-        <input type="button" value={trackstate} onClick={togglePlay} /> 
-        <input type="button" value="next" onClick={handleNext} />
-        <input type="button" value="previous" onClick={handlePrevious} />
+        <audio src={currentTrackUrl} ref={AudioRef} onTimeUpdate={(e)=>{setcurrentTime(e.currentTarget.currentTime)}} onDurationChange={(e)=>{setDuration(e.currentTarget.duration)}}/> 
+        
+        {/* <input type="button" value="previous" onClick={handlePrevious} /> */}
+        <button onClick={handlePrevious}>
+          <BiSkipPrevious/>
+        </button>
+        
+        {/* <input type="button" value={trackstate} onClick={togglePlay} /> */}
+        <button onClick={togglePlay}>
+          {trackstate=="play"? <BiPlay/> : currentTime==duration ? <BiPlay/> : <BiPause/>}
+        </button>
+        
+        {/* <input type="button" value="next" onClick={handleNext} /> */}
+        <button onClick={handleNext}>
+          <BiSkipNext/>
+        </button>
+        
+        <h1>duration : {duration}</h1>
+        <h1>current position : {Math.floor(currentTime)} seconds</h1>
+
     </div>
   )
 }
