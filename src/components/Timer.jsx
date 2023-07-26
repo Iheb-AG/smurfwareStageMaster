6197001fcc7fd35efc2abac421abbdcf4cb4d6b0
Iheb-AG <101937924+Iheb-AG@users.noreply.github.com>
2023-07-26 04:46:20 +0100

    updated timer logic

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -5,36 +5,50 @@ export default function Timer(){
   const [houres,setHoures] = useState("00")
   const [minutes, setMinutes] = useState("00")
   const [seconds, setSeconds] = useState("00") 
-  
+
+  const [reset,setReset] = useState(false)
+
   function handleInput(e){
-    setHoures(e.target.value.substring(4,6) ? e.target.value.substring(4,6)  : "00" )
+    setHoures(e.target.value.substring(4,6) ? e.target.value.substring(4,6)  : "00")
     setMinutes(e.target.value.substring(2,4) ? e.target.value.substring(2,4) : "00")
     setSeconds(e.target.value.substring(0,2) ? e.target.value.substring(0,2) : "00")
   }
-
 
   function handleTimerReset(e){
     setMinutes("00")
     setSeconds("00")
     setHoures("00")
+    setReset(true)
   }
 
   function handleTimerStart(e){
-    setInterval(() => {
-      
-    setSeconds(prevValue =>{
-      return prevValue !="00" ? String((parseInt(prevValue)-1)%60) : "00" 
-    })
+    e.preventDefault()
+    let time = parseInt(seconds) + parseInt(minutes)*60 + parseInt(houres)*3600
+
+    const timer = setInterval(() => {
+    //String((parseInt(prevValue)-1)%60)
+    // getting the full time in seconds
+
+    console.log("total seconds : ",time)
+
+    setSeconds(String(Math.floor(time%60)).length<2?"0"+String(Math.floor(time%60)) : String(Math.floor(time%60)) )
+    setMinutes(String(Math.floor((time%3600)/60)).length<2?"0"+String(Math.floor((time%3600)/60)) : String(Math.floor((time%3600)/60)))
+    setHoures(String(Math.floor(time/3600)).length<2?"0"+String(Math.floor(time/3600)):String(Math.floor(time/3600)))
     
-    setHoures(prevValue =>{
-      return prevValue !="00" ? String(parseInt(prevValue)-1) : "00" 
-    })
+    time = time-1
+    
+      if((time==-1)||(reset==true)){
+        time = 0
+        setHoures("00")
+        setMinutes("00")
+        setSeconds("00")
+        clearInterval(timer)
+      }
 
-    setMinutes(prevValue =>{
-      return prevValue !="00" ? String(parseInt(prevValue)-1) : "00" 
-    })
-
-  }, 1000);
+      setReset(false)
+    }, 1000);
+    
+    // setReset(false)
   }
   return (
     <div>
